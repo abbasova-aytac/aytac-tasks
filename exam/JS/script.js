d3f3ab5d243f5c25a8525ae61f0aa24a346fb4ea
abbasova-aytac <130342091+abbasova-aytac@users.noreply.github.com>
2023-05-11 18:52:18 +0400

    Add files via upload

---
+++ exam/JS/script.js
@@ -0,0 +1,46 @@
+// 3. Show number's dividends from 100 to 1000.
+// Input | Output
+// 50      | 100, 150, 200, 250, 300 ... 950
+// 70      | 140 , 210, 280, 980 ...
+// 85      | 170, 255, 340, ... 935
+
+    // let usernumber = +prompt("Enter a number's dividends")
+    // for (let i=100;i<1000;i++){  
+    // if(i % usernumber ==0) {
+    //     console.log (i)
+    // }
+    // }
+   
+
+// Show the count of the both odd and even numbers from array.
+// let numbers = [1,4,7,8,3,5,0,2,4,6,12,15,18,46,45,48,97,84]
+
+// For example, countEvensAndOdds(numbers);   // evens: 12, odds: 13
+
+// let numbers = [1,4,7,8,3,5,0,2,4,6,12,15,18,46,45,48,97,84]
+// let iseven=numbers.filter((num) => num %2===0 && num!==0);
+// console.log (iseven)
+// let isodd=numbers.filter((num) => num %2===1 );
+// console.log (isodd)
+
+
+
+// 4. We need to swap the case of all letters in a string. Provide us a function for doing it.
+// swapCase(string) : string
+// Input             |     Output
+// aBcD             |      AbCd
+// jAvaScRIPt     |     JaVAsCripT
+
+// function swapCase (string) {
+//     let swap = ""
+//     for(let i=0;i<string.length;i++){
+//      let word=string[i];
+//      if (word=== word.toLowerCase()){
+//      swap+=word.toUpperCase();}
+//      else if (word=== word.toUpperCase()){
+//      swap+=word.toLowerCase();}
+//      }
+//      return swap
+// }
+// console.log (swapCase("aBcD"))
+// console.log (swapCase("jAvaScRIPt"))
